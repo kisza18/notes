6acd7c87b373df96ce908b5d336bfc76070df630
Szabolcs Kiss <szabolcs17@gmail.com>
2023-05-04 22:29:11 +0200

    050402

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,17 @@
 import "./Note.css";
 
 const Note = (props) => {
+  const sendId = (msg) => {
+    props.deleteHandler(msg);
+  };
+
   return (
     <div className="note" style={{ background: props.color }}>
-      <img src="./images/trash.png" alt="" />
+      <img
+        src="./images/trash.png"
+        alt="trash"
+        onClick={() => sendId(props.keyId)}
+      />
       <p>{props.title}</p>
       <div className="note-container">
         <p>{props.tag}</p>
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -29,19 +29,39 @@ const NotesList = (props) => {
       date: "2023.05.04",
     },
   ]);
+
+  const checkNoteId = (id) => {
+    const filteredArray = notes.filter((note) => note.id !== id);
+    setNotes(filteredArray);
+  };
+
   return (
     <div className="notes-list">
-      {notes
-        .filter((note) => note.tag === props.tag)
-        .map((filteredNote) => (
-          <Note
-            key={filteredNote.id}
-            color={filteredNote.color}
-            title={filteredNote.title}
-            tag={filteredNote.tag}
-            date={filteredNote.date}
-          />
-        ))}
+      {props.tag === "all"
+        ? notes.map((note) => (
+            <Note
+              key={note.id}
+              keyId={note.id}
+              color={note.color}
+              title={note.title}
+              tag={note.tag}
+              date={note.date}
+              deleteHandler={checkNoteId}
+            />
+          ))
+        : notes
+            .filter((note) => note.tag === props.tag)
+            .map((filteredNote) => (
+              <Note
+                key={filteredNote.id}
+                keyId={filteredNote.id}
+                color={filteredNote.color}
+                title={filteredNote.title}
+                tag={filteredNote.tag}
+                date={filteredNote.date}
+                deleteHandler={checkNoteId}
+              />
+            ))}
     </div>
   );
 };
